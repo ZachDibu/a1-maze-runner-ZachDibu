011da7915d0f8195aba7a9d5109da64c7e14a124
owner <zachdibu@gmail.com>
2024-01-11 18:39:24 -0500

    react to -i (or --input) instead of always being the first argument.

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -13,8 +13,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("** Starting Maze Runner");
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            String inputFile = null;
+            for (int i = 0; i < args.length; i++){
+                if (args[i].equals("-i") || args[i].equals("--input")) {
+                    if (i + 1 < args.length) {
+                        inputFile = args[i + 1];
+                        break;
+                    }
+                }
+            }
+            System.out.println("**** Reading the maze from file " + inputFile);
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
